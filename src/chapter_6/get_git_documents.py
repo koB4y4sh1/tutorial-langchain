e113ea7bd2f9826de1d8e1ab3cd4df35f04b45dc
koB4y4sh1 <k_kobakenjp@yahoo.co.jp>
2025-08-14 03:55:50 +0900

    ファイルフィルタの設定変更
    より幅広いlangchainのドキュメントを検索対象に含めるため.mdxに変更

--- src/chapter_6/get_git_documents.py
+++ src/chapter_6/get_git_documents.py
@@ -2,15 +2,15 @@ from langchain_community.document_loaders import GitLoader
 from langchain_core.documents import Document
 
 def file_filter(file_path: str) -> bool:
-    return file_path.endswith("README.md")
+    return file_path.endswith(".mdx")
 
 def get_git_documents(url:str,repo_path:str)->list[Document]:
     """Gitリポジトリに保存されているファイルから、条件に一致するドキュメントを返却する"""
-    
+    # LangChain のドキュメントの増加により、gpt-4o-mini を使用してもmasterブランチにおいても、 Tier 1 ではエラーが発生することが報告されています。
     loader = GitLoader(
         clone_url=url,
         repo_path=repo_path,
-        branch="master",
+        branch="langchain==0.2.13",
         file_filter=file_filter,
     )
 
